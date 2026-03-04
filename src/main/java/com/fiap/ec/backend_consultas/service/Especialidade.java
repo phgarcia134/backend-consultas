aaed314060030da0ae3c4f54183ef774d6fe4875
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-03 22:10:01 -0300

    feat: criação do repository de Especialidade

--- src/main/java/com/fiap/ec/backend_consultas/service/Especialidade.java
+++ src/main/java/com/fiap/ec/backend_consultas/service/Especialidade.java
@@ -1,6 +1,5 @@
 package com.fiap.ec.backend_consultas.service;
 import com.fiap.ec.backend_consultas.model.Especialidade;
-import com.fiap.ec.backend_consultas.repository.EspecialidadeRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
