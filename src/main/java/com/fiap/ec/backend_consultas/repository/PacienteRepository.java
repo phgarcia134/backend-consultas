58dda63d643d1a9fc12a15235ac375bfa4c7f604
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-10 22:43:30 -0300

    test: validação dos endpoints de Paciente

--- src/main/java/com/fiap/ec/backend_consultas/repository/PacienteRepository.java
+++ src/main/java/com/fiap/ec/backend_consultas/repository/PacienteRepository.java
@@ -1,4 +1,5 @@
 package com.fiap.ec.backend_consultas.repository;
-
-public class PacienteRepository {
+import com.fiap.ec.backend_consultas.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
--- src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
+++ src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
@@ -1,4 +1,22 @@
 package com.fiap.ec.backend_consultas.service;
-
+import com.fiap.ec.backend_consultas.model.Paciente;
+import com.fiap.ec.backend_consultas.repository.PacienteRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
 public class PacienteService {
+    private final PacienteRepository repository;
+    public PacienteService(PacienteRepository repository) {
+        this.repository = repository;
+    }
+    public Paciente salvar(Paciente paciente) {
+        return repository.save(paciente);
+    }
+    public List<Paciente> listar() {
+        return repository.findAll();
+    }
+    public Paciente buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Paciente não encontrado"));
+    }
 }
