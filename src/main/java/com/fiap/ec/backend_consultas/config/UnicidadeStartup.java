fd48aad17a6826d83006ae1adb5934559ea67fb5
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-01 22:08:52 -0300

    aula01-09

---
+++ src/main/java/com/fiap/ec/backend_consultas/config/UnicidadeStartup.java
@@ -0,0 +1,5 @@
+package com.fiap.ec.backend_consultas.config;
+
+public class UnicidadeStartup {
+    
+}
---
+++ src/main/java/com/fiap/ec/backend_consultas/exception/DadosInvalidosException.java
@@ -0,0 +1,5 @@
+package com.fiap.ec.backend_consultas.exception;
+
+public class DadosInvalidosException {
+    
+}
